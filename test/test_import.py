d11dfba7e703da12234868c54fba40737df7725f
nzmattgrant <nzmattgrant@gmail.com>
2025-08-17 23:26:26 +0200

    test the dist

---
+++ test/test_import.py
@@ -0,0 +1,5 @@
+from epub_chapterize.chapterize import parse_epub
+
+print("EpubChapterize package imported successfully.")
+
+print(parse_epub("/Users/matthewgrant/Source/EpubChapterize/epub_chapterize/books/archive/Alice-In-Wonderland-En.epub"))
